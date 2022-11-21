2710c73f9b11d6c659b84ce916db7407afc25272
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2022-11-21 20:20:02 +0100

    Initial Template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Latex clone and run template
+
+Original devcontainer config from [@qdm12](https://github.com/qdm12/latexdevcontainer) and latex original template from @mariol03
---
+++ postbuild.sh
@@ -0,0 +1,14 @@
+tlmgr update --self
+tlmgr install babel-spanish
+tlmgr install xcolor
+tlmgr install tcolorbox
+tlmgr install pgf
+tlmgr install environ
+tlmgr install etoolbox
+tlmgr install listings
+tlmgr install listingsutf8
+tlmgr install parskip
+tlmgr install biblatex
+
+
+
